f7bac4e4eed0b090310effa38444681afc8cf4b6
youngq <yq5537@outlook.com>
2024-07-02 14:08:29 +0800

    更新

--- checkin.js
+++ checkin.js
@@ -1,8 +1,10 @@
 const axios = require('axios')
-const { getCookie, getTraffic, getEmailAndPwdList } = require('./utils')
+const { getCookie, getTraffic, getEmailAndPwdList,getCookieFy, getTrafficFy, getEmailAndPwdListFy } = require('./utils')
 const notify = require('./sendNotify')
 
 const checkinURL = 'https://ikuuu.pw/user/checkin'
+const checkinURLFy = 'https://a.lanying.win/user/checkin'
+
 
 /** 签到 */
 async function checkin(cookie) {
@@ -21,8 +23,27 @@ async function checkin(cookie) {
   }
 }
 
+/** 签到 */
+async function checkinFy(cookie) {
+  try {
+    const res = await axios(checkinURLFy, {
+      method: 'POST',
+      headers: {
+        Cookie: cookie
+      },
+      withCredentials: true
+    })
+    return res.data.msg
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+}
+
 async function run() {
   const [emailList, pwdList] = await getEmailAndPwdList()
+  
+  console.log('开始签到...',emailList,pwdList);
   const messages = []
   for (let i = 0; i < emailList.length; i++) {
     const email = emailList[i]
@@ -44,4 +65,31 @@ async function run() {
   await notify.sendNotify(`iKuuu VPN 签到通知`, messages.join('\n\n========================\n\n'))
 }
 
+async function runFy() {
+  const [emailList, pwdList] = await getEmailAndPwdListFy()
+  
+  console.log('开始签到fy...',emailList,pwdList);
+  const messages = []
+  for (let i = 0; i < emailList.length; i++) {
+    const email = emailList[i]
+    const pwd = pwdList[i]
+    let msg = `邮箱：${emailList[i]}`
+    const cookie = await getCookieFy(email, pwd)
+    if (cookie.includes('登录失败')) {
+      msg += `\n${cookie}`
+      messages.push(msg)
+      continue
+    }
+    const checkinRes = await checkinFy(cookie)
+    msg += `\n${checkinRes}`
+    const arr = await getTrafficFy(cookie)
+    msg += `\n${arr.join('\n')}`
+    messages.push(msg)
+  }
+
+  await notify.sendNotify(`飞云 VPN 签到通知`, messages.join('\n\n========================\n\n'))
+}
+
+
 run()
+runFy()
--- utils.js
+++ utils.js
@@ -1,6 +1,10 @@
 const { initInstance, getEnv } = require('./qlApi.js')
 const axios = require('axios')
 
+
+const loginURLFy = 'https://a.lanying.win/auth/login'
+const infoURLFy = 'https://a.lanying.win/user'
+
 const loginURL = 'https://ikuuu.pw/auth/login'
 const infoURL = 'https://ikuuu.pw/user'
 const todayTrafficReg = /今日已用\n.*\s(\d+\.?\d*)([M|G]?B)/
@@ -54,7 +58,45 @@ async function getEmailAndPwdList() {
 
   console.log(`✅ 成功读取 ${emailLen} 对邮箱和密码`)
 
-  return [emailList, pwdList]
+  return [emailList.map(x=>x.value), pwdList.map(x=>x.value)]
+}
+
+/** 获取邮箱和密码数组 飞云*/
+async function getEmailAndPwdListFy() {
+  let instance = null
+  try {
+    instance = await initInstance()
+  } catch (e) { }
+
+  let emailEnv = process.env.FYUN_EMAIL || []
+  let pwdEnv = process.env.FYUN_PWD || []
+
+  try {
+    if (instance) {
+      emailEnv = await getEnv(instance, 'FYUN_EMAIL')
+      pwdEnv = await getEnv(instance, 'FYUN_PWD')
+    }
+  } catch { }
+
+  const emailList = extractArr(emailEnv)
+  const pwdList = extractArr(pwdEnv)
+
+  const emailLen = emailList.length
+  const pwdLen = pwdList.length
+
+  if (!emailLen || !pwdLen) {
+    console.log('未获取到邮箱和密码, 程序终止')
+    process.exit(1)
+  }
+
+  if (emailLen !== pwdLen) {
+    console.log('邮箱和密码数量不一致, 程序终止')
+    process.exit(1)
+  }
+
+  console.log(`✅ 成功读取 ${emailLen} 对邮箱和密码`)
+
+  return [emailList.map(x=>x.value), pwdList.map(x=>x.value)]
 }
 
 /** 登录获取 cookie */
@@ -67,12 +109,32 @@ async function getCookie(email, pwd) {
       method: 'POST',
       data: formData
     })
+console.log(res.data)
     if (res.data.ret === 0) {
+        
       return `❌ 登录失败：${res.data.msg}`
     }
     console.log(`✅ 登录成功：${email}`)
     return res.headers['set-cookie'].join('; ')
   } catch { }
+}
+
+/** 登录获取 cookie  飞云*/
+async function getCookieFy(email, pwd) {
+  const formData = new FormData()
+  formData.append('email', email)
+  formData.append('passwd', pwd)
+  try {
+    const res = await axios(loginURLFy, {
+      method: 'POST',
+      data: formData
+    })
+    if (res.data.ret === 0) {
+      return `❌ 登录失败：${res.data.msg}`
+    }
+    console.log(`✅ 登录成功：${email}`)
+    return res.headers['set-cookie'].join('; ')
+  } catch (e){ console.log('获取cookie失败',e) }
 }
 
 /** 获取流量 */
@@ -105,4 +167,41 @@ async function getTraffic(cookie) {
   }
 }
 
-module.exports = { getCookie, getTraffic, getEmailAndPwdList }
+
+/** 获取流量 飞云 */
+async function getTrafficFy(cookie) {
+  try {
+    const { data } = await axios(infoURLFy, {
+      method: 'GET',
+      headers: {
+        Cookie: cookie
+      },
+      withCredentials: true
+    })
+
+    // const trafficRes = data.match(todayTrafficReg)
+    // const restRes = data.match(restTrafficReg)
+    // if (!trafficRes || !restRes) {
+    //   return ['查询流量失败，请检查正则和用户页面 HTML 结构']
+    // }
+
+    // const [, today, todayUnit] = trafficRes
+    // const [, rest, restUnit] = restRes
+
+    const trafficRes = data.match(/<span[^>]*>剩余流量 ： ([^<]+)<\/span>/)
+if (!trafficRes) {
+  return ['查询流量失败，请检查正则和用户页面 HTML 结构']
+}
+const [, today] = trafficRes
+console.log(today);
+    return [
+      // `今日已用：${today} ${todayUnit}`,
+      `剩余流量：${today.trim()}`
+    ]
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+}
+
+module.exports = { getCookie, getTraffic, getEmailAndPwdList,getCookieFy, getTrafficFy, getEmailAndPwdListFy }
